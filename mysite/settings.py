105d74f66c5e0c452d8cce02795942d9c9360d0d
sachin <sachin@gmail.com>
2025-09-12 15:07:25 +0530

    updated files

--- mysite/settings.py
+++ mysite/settings.py
@@ -159,5 +159,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sainisachin91738@gmail.com'
-DEFAULT_FROM_EMAIL = 'sainisachin91738@gmail.com'
+EMAIL_HOST_USER = 'sachinsaini96644@gmail.com'
+DEFAULT_FROM_EMAIL = 'sachinsaini96644@gmail.com'
