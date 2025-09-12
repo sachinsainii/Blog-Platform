2cbbbe20cd33b94db75e2448a315a1844a3221fd
sachin <sachin@gmail.com>
2025-09-12 17:27:22 +0530

    updated files

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,6 +99,12 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'myblogdb',
+        # 'USER': 'mybloguser',
+        # 'PASSWORD': 'mypassword',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
